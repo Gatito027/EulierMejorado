fc6e17d3a16ecde979fc237178c9715f095abcf4
Gatito027 <dddanieeel@hotmail.com>
2025-03-29 12:10:48 -0600

    Arreglando cosas

--- app.py
+++ app.py
@@ -10,39 +10,17 @@ def home():
 def calcular():
     # Obtener datos enviados en el cuerpo de la solicitud
     datos = request.get_json()
-    _ecuacion = datos.get("Ecuacion")
-    _y = datos.get("valorY")
-    _x = datos.get("valorX")
-    _n = datos.get("valorN")
+    ecuacion = datos.get("Ecuacion")
+    y = datos.get("valorY")
+    x = datos.get("valorX")
+    h = datos.get("valorH")
     # Validar que los datos existan
     if not datos:
         return jsonify({"error": "No se enviaron parámetros"}), 400
-    # Ejemplo: Retornar los mismos datos en formato JSON
-    convertido= obtenerNumeros(_ecuacion)
-    tipo = clasificarEcuacion(_ecuacion)
     return jsonify({
-        "Void":tipo
+        "Void":"nada"
     })
 
-def obtenerNumeros(_ecuacion):
-    # Usamos una comprensión de lista para filtrar solo los dígitos
-    numeros = ''.join([caracter for caracter in _ecuacion if caracter.isdigit()])
-    return numeros
-
-def clasificarEcuacion(ecuacion):
-    # Verificar si la ecuación contiene variables 'x' y 'y'
-    if "x" in ecuacion and "y" in ecuacion:
-        # Verificar si la ecuación incluye multiplicaciones simples (linealidad)
-        if "*" not in ecuacion and "^" not in ecuacion:
-            return "lineal"
-        # Si hay multiplicación y otros términos complejos, es no lineal
-        elif "*" in ecuacion or "^" in ecuacion:
-            return "no lineal"
-        else:
-            return "desconocida"
-    else:
-        # Si no contiene las variables necesarias, es desconocida
-        return "desconocida"
 
 
 
